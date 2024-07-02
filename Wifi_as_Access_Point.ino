3ae9116e2af15cf737c81adf628316056387848c
Shakaib-Naqvi <shakaibnaqvi401@gmail.com>
2024-07-02 16:58:44 +0500

    first commit

---
+++ Wifi_as_Access_Point.ino
@@ -0,0 +1,18 @@
+#include <WiFi.h>
+#include "esp_wifi.h"
+
+
+const char* ssid     = "ESP32";
+const char* password = "123456789";
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("\n[*] Creating AP");
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.print("[+] AP Created with IP Gateway ");
+    Serial.println(WiFi.softAPIP());
+}
+
+void loop(){}
